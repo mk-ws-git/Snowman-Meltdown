1c10596264daac71aa14449cf5fc14de878fdd44
mk-ws-git <maryksprague@gmail.com>
2026-02-02 19:10:26 +0100

    Update ASCII art with smoother melt stages

--- ascii_art.py
+++ ascii_art.py
@@ -1,30 +1,47 @@
-# Snowman ASCII Art stages
 STAGES = [
     # Stage 0: Full snowman
     """
      ___
     /___\\
     (o o)
+   \\( : )/
     ( : )
-    ( : )
-    """,
-    # Stage 1: Bottom part starts melting
-    """
-     ___
-    /___\\
-    (o o)
-    ( : )
-    """,
-    # Stage 2: Only the head remains
-    """
-     ___
-    /___\\
-    (o o)
-    """,
-    # Stage 3: Snowman completely melted
-    """
-     ___
-    /___\\
-    """
-]
+     - -
+     """,
 
+    # Stage 1: Bottom melting
+    """
+     ___
+    /___\\
+    (o o)
+   \\( : )/
+     """,
+
+    # Stage 2: Body melting
+    """
+     ___
+    /___\\
+    (o o)
+    (   )
+     """,
+
+    # Stage 3: Body gone
+    """
+     ___
+    /___\\
+    (o o)
+     """,
+
+    # Stage 4: Head sinking
+    """
+     ___
+    /___\\
+     o o
+     """,
+
+    # Stage 5: Completely melted
+    """
+     ___
+    /___\\
+     """
+]
